f8319d7340f0c8c459cb353cb11bfc19e04a47d2
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-10 19:22:06 -0400

    exercise 2-24 searching array of object

--- exercises/arrayscripts/actors.js
+++ exercises/arrayscripts/actors.js
@@ -20,6 +20,7 @@ let academyMembers = [
     films: ["Walking Boots", "Hiking Boots", "Cowboy Boots"],
   },
 ];
+// Who is the Academy Member whose ID is 187?
 
 function getMemberById(members, memberId) {
   for (const member of members) {
@@ -36,3 +37,63 @@ function printMemberToConsole(member) {
 let matchingMember = getMemberById(academyMembers, 187);
 let message = printMemberToConsole(matchingMember);
 console.log(message);
+
+// Who has have been in at least 3 films?
+let resultArray = [];
+function getMembersIn3Films(members) {
+  for (const member of members) {
+    if (member.films.length >= 3) {
+      resultArray.push(member.name);
+    }
+  }
+  return resultArray;
+}
+console.log("Who has have been in at least 3 films");
+let memberlist = getMembersIn3Films(academyMembers);
+for (const memberName of memberlist) {
+  console.log(memberName);
+}
+// Who has a name that starts with "Bob"?
+
+let resultArray1 = [];
+function getMemberStartswith(members) {
+  for (const member of members) {
+    if (member.name.startsWith("Bob")) {
+      resultArray1.push(member.name);
+    }
+  }
+  return resultArray1;
+}
+console.log("Who has a name that starts with Bob");
+let list1 = getMemberStartswith(academyMembers);
+for (const memberName of list1) {
+  console.log(memberName);
+}
+// HARDER: Which Academy Members have been in a film that starts with "A"
+
+let resultArray2 = [];
+function getMemberFilmFilter(members) {
+  for (const member of members) {
+
+    // member ={
+    //   memID: 142,
+    //   name: "Sallie Smith",
+    //   films: ["A Good Day", "A Better Day"],
+    // },
+    for (const filmname of member.films) {
+      // filmname="A Good Day",
+      if (filmname.startsWith("A")) {
+        resultArray2.push(member.name);
+          break;
+      }
+    }
+  }
+  return resultArray2;
+}
+console.log(
+  "// HARDER: Which Academy Members have been in a film that starts with A"
+);
+let list2 = getMemberFilmFilter(academyMembers);
+for (const memberName of list2) {
+  console.log(memberName);
+}
