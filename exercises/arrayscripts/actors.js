edc9d71b450ccf5a359ac04594b654e1dd3435a1
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-10 09:27:29 -0400

    exercise of page no 2-23

---
+++ exercises/arrayscripts/actors.js
@@ -0,0 +1,40 @@
+let academyMembers = [
+    {
+    memID: 101,
+    name: "Bob Brown",
+    films: ["Bob I", "Bob II", "Bob III", "Bob IV"]
+    },
+    {
+    memID: 142,
+    name: "Sallie Smith",
+    films: ["A Good Day", "A Better Day"]
+    },
+    {
+    memID: 187,
+    name: "Fred Flanders",
+    films: ["Who is Fred?", "Where is Fred?",
+    "What is Fred?", "Why Fred?"]
+    },
+    {
+    memID: 203,
+    name: "Bobbie Boots",
+    films: ["Walking Boots", "Hiking Boots",
+    "Cowboy Boots"]
+    },
+   ];
+
+   function getMemberById(members, memberId) {
+    for (const member of members) {
+      if (member.memID == memberId) {
+        return member;
+      }
+    }
+  }
+  
+  function printMemberToConsole(member) {
+      return `The member with id: ${member.memID} is ${member.name} `;
+  }
+  
+  let matchingMember = getMemberById(academyMembers, 142);
+  let message = printMemberToConsole(matchingMember);
+  console.log(message);
