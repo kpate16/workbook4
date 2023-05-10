97b36e6574143d026b5a46a52f66c8fbe08cff9f
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-10 14:58:30 -0400

    sorting array

---
+++ example/2-28_sorting_array_number.js
@@ -0,0 +1,11 @@
+function compareAscendingNumber(a, b) {
+    if (a < b) return -1; // right order
+    else if (a == b) return 0; // same values
+    else return 1; // swap, wrong order
+   }
+   
+
+let numbers = [3, 27, 400, 1, 111, 5];
+compareAscendingNumber(27,400)
+numbers.sort(compareAscendingNumber);
+console.log(numbers);
